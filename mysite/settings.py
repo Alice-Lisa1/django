bce6e7e9af1ad3360557d098dbd11f73e9292c2a
Alice-Lisa1 <alisadrobnay@gmail.com>
2025-02-05 17:23:57 +0500

    adding a error3

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
